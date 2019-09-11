11433954a58f4527c9ee03913709c04ccc0b8065
alek-wi <55146263+alek-wi@users.noreply.github.com>
2019-09-11 20:25:20 +0200

    wersja1

---
+++ GraZaDużoZaMalo/Monolitycznie/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1. komputer losuje liczbę z podanego zakresu
+
+            Random los = new Random();
+            int wylosowana = los.Next(1, 101);
+#if DEBUG
+            Console.WriteLine(wylosowana); //do usuniecia w Release
+#endif
+
+            //powtarzaj wielokrotnie, aż odgadnie
+            //2. człowiek proponuje (odgaduje)
+
+            //3. komputer ocenia propozycję
+            //koniec powtarzaj
+
+            //4. wypisz statystyki gry
+
+
+
+        }
+    }
+}
