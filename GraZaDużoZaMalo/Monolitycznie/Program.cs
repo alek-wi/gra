8d01bd935557f24d29bf79c7ab984bcfa3e327e6
alek-wi <55146263+alek-wi@users.noreply.github.com>
2019-09-12 20:19:39 +0200

    gra za dużo za mało czwartek

--- GraZaDużoZaMalo/Monolitycznie/Program.cs
+++ GraZaDużoZaMalo/Monolitycznie/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Monolitycznie
 {
@@ -18,17 +19,81 @@ namespace Monolitycznie
 #if DEBUG
             Console.WriteLine(wylosowana); //do usuniecia w Release
 #endif
+            Console.WriteLine("wylosowałem liczbe z zakresu od 1 do 100. Odgadni ją.");
+            Stopwatch czas = Stopwatch.StartNew();
 
             //powtarzaj wielokrotnie, aż odgadnie
-            //2. człowiek proponuje (odgaduje)
+            bool odgadniete = false;
+            int licznik = 0;
 
-            //3. komputer ocenia propozycję
-            //koniec powtarzaj
+            do
+            {
+                licznik++;
+                //2. człowiek proponuje (odgaduje)
+                Console.Write("podaj swoją propozycję: ");
+                string napis = Console.ReadLine();
+                if (napis == "koniec")
 
+                {
+                    Console.WriteLine("szkoda, że mnie opuzczasz.");
+                    return;
+                }
+
+                int propozycja = 0;
+                try
+                {
+                    propozycja = int.Parse(napis);
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Nie podano liczby.\n Spróbuj jeszcze raz");
+                    continue;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Przesadziłeś, za duża liczba.");
+                    continue;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("niezidentyfikowany wyjątek. Awaria !!");
+                    Environment.Exit(1);
+                }
+                
+         
+                //3. komputer ocenia propozycję
+                //koniec powtarzaj
+                if (propozycja < wylosowana)
+                {
+                    Console.WriteLine("Za mało");
+                }
+                else if (propozycja > wylosowana)
+                {
+                    Console.WriteLine("Za dużo");
+                }
+                else
+                {
+                    Console.WriteLine("Trafiłeś");
+                    //odgadniete = true;
+                    break;
+                }
+            }
+            //while (!odgadniete); while(propozycja !=wylosowana5)
+            while (true);
+
+            czas.Stop();
             //4. wypisz statystyki gry
 
 
+            Console.WriteLine($"*liczba ruchow{licznik}");
+            Console.WriteLine($"-czas gry:{czas.Elapsed}");
+                
+            
 
+
+
+            
         }
     }
 }
